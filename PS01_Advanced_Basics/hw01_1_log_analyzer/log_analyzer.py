4b0bab89f87388668c21f2a8a8c47808a624ca86
Alexander <43119129+AlexandrNikitin3776@users.noreply.github.com>
2023-08-15 09:27:56 +0300

    Add config structure

--- PS01_Advanced_Basics/hw01_1_log_analyzer/log_analyzer.py
+++ PS01_Advanced_Basics/hw01_1_log_analyzer/log_analyzer.py
@@ -7,11 +7,39 @@
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
 
+from dataclasses import dataclass
+from typing import TypeAlias
+
 config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
+
+FilePath: TypeAlias = str
+LogDir: TupeAlias = str
+ReportDir: TypeAlias = str
+ReportSize: TypeAlias = str
+
+
+@dataclass
+class Config:
+    report_size: ReportSize
+    report_dir: ReportDir
+    log_dir: LogDir
+
+    @classmethod
+    def from_file(cls; type[Self], fileobj: IO) -> Self:
+        ...
+
+
+def get_config() -> Config:
+    ...
+
+
+def get_last_log_file(log_dir: LogDir) -> FilePath:
+    ...
 
 
 def main():
-    pass
+    config = get_config()
+    last_log_file = get_last_log_file(config.log_dir)
 
 
 if __name__ == "__main__":
