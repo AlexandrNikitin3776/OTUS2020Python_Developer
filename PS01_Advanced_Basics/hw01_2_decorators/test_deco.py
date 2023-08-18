a44f00f1af1a9e77ff5071a650264f4e13f06464
Alexander Nikitin <snegopad1@gmail.com>
2023-08-18 11:12:24 +0300

    Fix tests

--- PS01_Advanced_Basics/hw01_2_decorators/test_deco.py
+++ PS01_Advanced_Basics/hw01_2_decorators/test_deco.py
@@ -6,7 +6,7 @@ def test_foo():
     print(foo(4, 3, 2))
     print(foo(4, 3))
     print("foo was called", foo.calls, "times")
-    assert foo.calls == 3
+    assert foo.calls == 2
 
 
 def test_bar():
@@ -14,11 +14,11 @@ def test_bar():
     print(bar(4, 3, 2))
     print(bar(4, 3, 2, 1))
     print("bar was called", bar.calls, "times")
-    assert bar.calls == 5
+    assert bar.calls == 3
 
 
 def test_fib():
     print(fib.__doc__)
     fib(4)
     print(fib.calls, "calls made")
-    assert fib.calls == 6
+    assert fib.calls == 7
