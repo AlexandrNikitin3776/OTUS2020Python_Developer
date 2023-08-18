9009443ecdd1a24a7dba9be618a4f11d97116a45
Alexander <43119129+AlexandrNikitin3776@users.noreply.github.com>
2023-08-18 11:04:30 +0300

    Create test_deco.py

---
+++ PS01_Advanced_Basics/hw01_2_decorators/test_deco.py
@@ -0,0 +1,24 @@
+from deco import foo, bar, fib
+
+
+def test_foo():
+    print(foo(4, 3))
+    print(foo(4, 3, 2))
+    print(foo(4, 3))
+    print("foo was called", foo.calls, "times")
+    assert foo.calls == 3
+
+
+def test_bar():
+    print(bar(4, 3))
+    print(bar(4, 3, 2))
+    print(bar(4, 3, 2, 1))
+    print("bar was called", bar.calls, "times")
+    assert bar.calls == 5
+
+
+def test_fib():
+    print(fib.__doc__)
+    fib(4)
+    print(fib.calls, "calls made")
+    assert fib.calls == 6
