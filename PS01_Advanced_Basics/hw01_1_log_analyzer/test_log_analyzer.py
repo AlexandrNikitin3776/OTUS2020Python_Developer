5d21fce218fd68e106911c1bc6933776fc93550b
Alexander <43119129+AlexandrNikitin3776@users.noreply.github.com>
2023-08-20 18:55:24 +0300

    Update test_log_analyzer.py

--- PS01_Advanced_Basics/hw01_1_log_analyzer/test_log_analyzer.py
+++ PS01_Advanced_Basics/hw01_1_log_analyzer/test_log_analyzer.py
@@ -1,5 +1,17 @@
+import io
 from log_analyzer import Config
+import tempfile
 
 
 def test_config():
-    assert False
+    file = io.StringIO("""[ini]""")
+    
+    got_config = Config.from_file(file)
+    
+    assert get_config == Config()
+
+
+def test_get_last_log_file():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with tempfile.NamedTemporaryFile(dir=tmpdir) tmpfile:
+            assert False
